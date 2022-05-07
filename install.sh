739d6513fe70da133dece346032bbaf3249438ab
keishi_ishihara <keishi_ishihara@albert2005.co.jp>
2022-05-07 21:24:15 +0900

    update installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 
-function detect_shell() { echo ${SHELL##*/}; }
-function echo_header() { printf "\033[37;1m%s\033[m\n" "$*"; }
-function is_exists() { type "$1" >/dev/null 2>&1; return $?; }
-function _unlink() { unlink "$1" >/dev/null 2>&1; return $?; }
-function _mv() { mv "$1" >/dev/null 2>&1; return $?; }
-function _mkdir() { mkdir "$1" >/dev/null 2>&1; return $?; }
+source ./scripts/utils.sh
 
 _unlink ~/.bashrc
 _unlink ~/.zshrc
