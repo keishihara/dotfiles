aa37df37e9657a327da485410b370b52c91667f8
keishi_ishihara <keishi_ishihara@albert2005.co.jp>
2022-11-12 17:28:13 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-source ./scripts/utils.sh
+function detect_shell() { echo ${SHELL##*/}; }
+function _unlink() { unlink "$1" >/dev/null 2>&1; return $?; }
+function _mkdir() { mkdir "$1" >/dev/null 2>&1; return $?; }
+function _mv() { mv "$1" >/dev/null 2>&1; return $?; }
+
 
 _unlink ~/.bashrc
 _unlink ~/.zshrc
 _unlink ~/.gitconfig
 _unlink ~/.tmux.conf
 _unlink ~/.aliases
+_unlink ~/.utils
 
-_mkdir dotbackup
-_mv ~/.bashrc dotbackup
-_mv ~/.zshrc dotbackup
-_mv ~/.gitconfig dotbackup
-_mv ~/.tmux.conf dotbackup
-_mv ~/.aliases dotbackup
+_mkdir backup
+_mv ~/.bashrc backup
+_mv ~/.zshrc backup
+_mv ~/.gitconfig backup
+_mv ~/.tmux.conf backup
+_mv ~/.aliases backup
+_mv ~/.utils backup
 
 ln -s ~/dotfiles/.aliases ~
+ln -s ~/dotfiles/.utils ~
 ln -s ~/dotfiles/.gitconfig ~
 ln -s ~/dotfiles/.tmux.conf ~
 
@@ -26,5 +33,5 @@ elif [ "$(detect_shell)" = bash ]; then
     ln -s ~/dotfiles/.bashrc ~
     source ~/.bashrc
 else
-    echo_header "zsh and bash only supported: $(detect_shell)"
+    echo_header "zsh and bash are only supported: $(detect_shell)"
 fi
