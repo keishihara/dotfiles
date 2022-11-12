efd8f213a263f56a0da8916ed9eb5d04c2d20f16
keishi_ishihara <keishi_ishihara@albert2005.co.jp>
2022-11-12 17:26:17 +0900

    delete

--- scripts/utils.sh
+++
@@ -1,6 +0,0 @@
-function detect_shell() { echo ${SHELL##*/}; }
-function echo_header() { printf "\033[37;1m%s\033[m\n" "$*"; }
-function is_exists() { type "$1" >/dev/null 2>&1; return $?; }
-function _unlink() { unlink "$1" >/dev/null 2>&1; return $?; }
-function _mv() { mv "$1" >/dev/null 2>&1; return $?; }
-function _mkdir() { mkdir "$1" >/dev/null 2>&1; return $?; }
