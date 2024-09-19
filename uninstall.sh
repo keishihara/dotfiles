193b7df354f7bec1fb9235978336ee22f3362ed1
keishi.ishihara <keishi.ishihara17@gmail.com>
2024-09-19 13:36:19 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 unlink ~/.bashrc
+unlink ~/.zshenv
+unlink ~/.zprofile
 unlink ~/.zshrc
 unlink ~/.gitconfig
 unlink ~/.tmux.conf
-unlink ~/.aliases
+unlink ~/.Brewfile
+unlink ~/.wezterm.lua
+unlink ~/.vimrc
 
 # Re-login to interactive shell
 exec $SHELL -l
